5abaf501e6feb3b8f1cdade8bc040d95b8856a76
Ilia-Minaev <mi1127526540@gmail.com>
2023-09-04 19:00:03 -0300

    header and fooret menu

--- webar/blog/templatetags/menu_tags.py
+++ webar/blog/templatetags/menu_tags.py
@@ -3,9 +3,13 @@ from blog.models import Page
 register = template.Library()
 
 
-@register.inclusion_tag('blog/tags/menu.html', takes_context=True)
-def show_top_menu(context):
+@register.inclusion_tag('blog/tags/header_menu.html', takes_context=True)
+def show_header_menu(context):
     menu_items = Page.objects.all()
-    return {
-        "menu_items": menu_items,
-    }
+    return {"menu_items": menu_items,}
+
+@register.inclusion_tag('blog/tags/footer_menu.html', takes_context=True)
+def show_footer_menu(context):
+    menu_items = Page.objects.all()
+    return {"menu_items": menu_items,}
+
